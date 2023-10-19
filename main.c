115d6dbe16480626793b00110f8ab5ab9499421a
LOVEKOREAN <JIWON0521@SOOKMYUNG.AC.KR>
2023-10-19 15:45:16 +0900

    03
    실습3

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int inc(int counter);
+int main(int argc, char *argv[])
 {
-  int i;
+  int i = 10;
+  printf("함수 호출 전 i=%d\n", i);
+  i= inc(i);
+  printf("함수 호출 후 i=%d\n", i);
   
-  for (i=0; i<5; i++)
-  {
-      static int temp =1;
-      printf("temp = %d\n", temp);
-      temp++;
-   }
-   
   system("PAUSE");	
   return 0;
+
 }
+
+int inc(int counter)
+{
+    counter++;
+    return counter;
+} 
+  
+   
+  
  
  
