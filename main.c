73d17189337160fe449b7f483efec2b2e4ab9629
LOVEKOREAN <JIWON0521@SOOKMYUNG.AC.KR>
2023-10-19 15:19:45 +0900

    01
    실습1번

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int flag=1;
+  
+  while(flag !=0)
+  {
+             int y;
+             y=3;
+             flag=0;
+   }
+   
+   printf("y = %d",y);
+   
+  system("PAUSE");	
+  return 0;
+}
+ 
